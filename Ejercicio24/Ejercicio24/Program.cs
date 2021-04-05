39524ea155e2d2b388a4a5e58fcfe70fecf9a1c2
diego.taub <diego.taub@accenture.com>
2021-04-04 21:08:13 -0300

    Ejercicio24

---
+++ Ejercicio24/Ejercicio24/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese una fecha con el formato dd/MM/yyyy: ");
+            if(!DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", 
+                System.Globalization.CultureInfo.InvariantCulture,
+                System.Globalization.DateTimeStyles.None, out DateTime Fecha))
+            {
+                Console.Write("La fecha no tiene formato dd/MM/yyyy");
+            }
+            else
+            {
+                Console.Write("Fecha correcta");
+            }
+            Console.ReadKey();
+        }
+    }
+}
